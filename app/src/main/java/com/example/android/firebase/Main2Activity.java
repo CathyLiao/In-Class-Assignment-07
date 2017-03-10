08bbe4c4b6a12a6b8ff863feff8c4849a5e75001
CathyLiao <Jingyuan.Liao@tc.columbia.edu>
2017-03-09 19:41:17 -0500

    add userSecret

---
+++ app/src/main/java/com/example/android/firebase/Main2Activity.java
@@ -0,0 +1,44 @@
+package com.example.android.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("secret");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+
+                TextView textview = (TextView)findViewById(R.id.secretDisplay);
+                textview.setText(value);
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/android/firebase/MainActivity.java
+++ app/src/main/java/com/example/android/firebase/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.android.firebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void save(View view) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("secret");
+
+        EditText user = (EditText) findViewById(R.id.name);
+        String username  = user.getText().toString();
+
+        DatabaseReference myRef = database.getReference(username).child("secret");
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String userSecret  = editText.getText().toString();
+
+        myRef.setValue(userSecret);
+    }
+
+    public void reveal(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
 }
